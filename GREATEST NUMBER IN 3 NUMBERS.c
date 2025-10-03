b9a04aca200cbc0afd1f9f3371e847cdf16604e2
Ajay Yadav <ayajay2006@gmail.com>
2025-10-03 06:02:33 +0530

    Update GREATEST NUMBER IN 3 NUMBERS.c

--- GREATEST NUMBER IN 3 NUMBERS.c
+++ GREATEST NUMBER IN 3 NUMBERS.c
@@ -1,36 +1,35 @@
-# include <stdio.h>
+#include <stdio.h>
 
 int main()
 {
-	float a,b,c;
-	
-	printf("ENTER FIRST NUMBER :");
-	scanf("%f", &a);
-	
-	printf("ENTER SECOND NUMBER :");
-	scanf("%f", &b);
-	
-	printf("ENTER THIRD NUMBER :");
-	scanf("%f", &c);
-	
-	if(a>b && a>c)
-	{
-	printf("GREATEST NUMBER IS %f", a);
-	}
-	
-	else
-	{
-	if(b>a && b>c)
-	{
-	printf("GREATEST NUMBER IS %f", b);
-	}
-	
-	else
-	{
-		printf("GREASTEST NUMBER IS %f" , c);
-	}
-	
-	}
-	
-	return 0;
+    float a, b, c;
+
+    printf("ENTER FIRST NUMBER :");
+    scanf("%f", &a);
+
+    printf("ENTER SECOND NUMBER :");
+    scanf("%f", &b);
+
+    printf("ENTER THIRD NUMBER :");
+    scanf("%f", &c);
+
+    if (a > b && a > c)
+    {
+        printf("GREATEST NUMBER IS %f", a);
+    }
+
+    else
+    {
+        if (b > a && b > c)
+        {
+            printf("GREATEST NUMBER IS %f", b);
+        }
+
+        else
+        {
+            printf("GREASTEST NUMBER IS %f", c);
+        }
+    }
+
+    return 0;
 }
