f2ef84efe4824b0faf15358bde66953e96da652c
Ajay Yadav <ayajay2006@gmail.com>
2025-10-03 06:01:50 +0530

    Update COMPOUND INTEREST.cpp

--- COMPOUND INTEREST.cpp
+++ COMPOUND INTEREST.cpp
@@ -1,28 +1,27 @@
 #include <stdio.h>
-#include<math.h>
+#include <math.h>
 int main()
 {
-	float a,b,c,d,e,f,g,h;
+    float a, b, c, d, e, f, g, h;
 
     printf("ENTER PRINCIPAL AMMOUNT :");
-    scanf("%f",&a);
+    scanf("%f", &a);
 
-	printf("ENTER RATE :");
-    scanf("%f",&b);
+    printf("ENTER RATE :");
+    scanf("%f", &b);
 
     printf("ENTER TIME (IN YEARS) :");
-    scanf("%f",&c);
-    
+    scanf("%f", &c);
+
     printf("ENTER NUMBER OF TIME FOR WHICH INTEREST COMPOUNDED IN A YEAR :");
-    scanf("%f",&d);
-    
-    e=1+ ((b/d)/100);
-    f=pow(e,d*c);
-    g=a*f;
-    h=g-a;
-	
-	printf("COMPOUNT INTEREST OF %f YEARS IS %f",c,h);
-	
-	printf("MY NAME IS AJAY YADAV \n");
-	return 0;
+    scanf("%f", &d);
+
+    e = 1 + ((b / d) / 100);
+    f = pow(e, d * c);
+    g = a * f;
+    h = g - a;
+
+    printf("COMPOUNT INTEREST OF %f YEARS IS %f", c, h);
+
+    return 0;
 }
